84a2fe4545de72306fb34a436991fa969a5ae1dd
ozyurtf <ozyurtf@outlook.com>
2022-07-30 13:33:22 -0400

    adding explanations

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+# Importing libraries.
 import json
 
+# Opening a sample json file.
 f = open('/Users/ozyurtf/Documents/data/AI4Code/train/0a0b0188aa37ca.json')
-
 data = json.load(f)
-
 f.close()
 
+# Separating codes and markdowns.
 code_ids = []
 markdown_ids = []
 for k in data['cell_type'].keys():
@@ -15,6 +16,7 @@ for k in data['cell_type'].keys():
     else:
         markdown_ids.append(k)
 
+# Printing codes and markdowns.
 print("Codes:")
 print("\n".join([data['source'][val] for val in code_ids]))
 print()
