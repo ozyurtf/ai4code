9fae2b8490f8d60cfdfc1c57e721e1eaa7609c1f
ozyurtf <ozyurtf@outlook.com>
2022-07-31 15:52:52 -0400

    integrating codes and markdowns into the existing training data.

--- main.py
+++ main.py
@@ -2,65 +2,43 @@
 import json
 import pandas as pd
 
-# Definin the main path.
+# Defining the main path.
 main_path = '/Users/ozyurtf/Documents/data/ai4code/'
 
 # Importing data.
 train_ancestors = pd.read_csv(main_path + 'train_ancestors.csv')
-train_orders = pd.read_csv(main_path + '/train_orders.csv')
-
-# Sample ID in training data.
-train_sample_id = '00015c83e2717b'
-
-# Opening a sample training json file.
-with open(main_path + 'train/{}.json'.format(train_sample_id)) as f:
-    train_data = json.load(f)
-
-# Printing codes and markdowns.
-print("Codes:")
-print('-------------------------------------------------------------')
-print("\n".join([train_data['source'][val] for val in code_ids]))
-print()
-print('-------------------------------------------------------------')
-print("Markdowns:")
-print('-------------------------------------------------------------')
-print("\n".join([train_data['source'][val] for val in markdown_ids]))
-print()
-print('-------------------------------------------------------------')
-print("Cell Order:")
-print('-------------------------------------------------------------')
-cell_order = train_orders[train_orders['id'].apply(lambda x: x == '00015c83e2717b')]['cell_order'].values[0].split(' ')
-print('\n'.join([train_data['source'][val] for val in cell_order]))
-print('-------------------------------------------------------------')
-
-# Sample ID in test data.
-test_sample_id = '0010a919d60e4f'
-
-# Opening a sample training json file.
-with open(main_path + 'test/{}.json'.format(test_sample_id)) as f:
-    test_data = json.load(f)
+train_orders = pd.read_csv(main_path + 'train_orders.csv')
 
 
-# Separating codes and markdowns.
-code_ids = []
-markdown_ids = []
-for k in test_data['cell_type'].keys():
-    cell_type = test_data['cell_type'][k]
-    if cell_type=='code':
-        code_ids.append(k)
-    else:
-        markdown_ids.append(k)
+def extract_codes_markdowns(train_or_test, code_or_markdown, file_id):
+    # Importing data.
+    with open(main_path + '{}/{}.json'.format(train_or_test, file_id)) as f:
+        data = json.load(f)
+
+    # Extracting codes/markdowns.
+    code_or_markdown_list = []
+    for k in data['cell_type'].keys():
+        cell_type = data['cell_type'][k]
+        if cell_type == code_or_markdown:
+            code_or_markdown_list.append(data['source'][k])
+        else:
+            pass
+    # Returning the list of codes/markdowns.
+    return code_or_markdown_list
 
 
-# Printing codes and markdowns.
-print("Codes:")
-print('-------------------------------------------------------------')
-print("\n".join([test_data['source'][val] for val in code_ids]))
-print()
-print('-------------------------------------------------------------')
-print("Shuffled Markdowns:")
-print('-------------------------------------------------------------')
-print("\n".join([test_data['source'][val] for val in markdown_ids]))
-print()
-print('-------------------------------------------------------------')
+# Extracting the codes in training data.
+train_codes = train_orders['id'].apply(lambda x: extract_codes_markdowns(train_or_test='train',
+                                                                         code_or_markdown='code',
+                                                                         file_id=x))
 
+# Extracting the markdowns in training data
+train_markdowns = train_orders['id'].apply(lambda x: extract_codes_markdowns(train_or_test='train',
+                                                                             code_or_markdown='markdown',
+                                                                             file_id=x))
+
+# Including codes and markdowns in training data.
+train_codes_markdowns = pd.concat([train_codes, train_markdowns], axis=1)
+train_codes_markdowns.columns = ['codes', 'markdowns']
+train_final = pd.concat([train_orders, train_codes_markdowns], axis=1)
+train_final = train_final[['id', 'codes', 'markdowns', 'cell_order']]
