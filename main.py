c95d16282f10fd1c7c5d3ef5004422ec82a6af5e
ozyurtf <ozyurtf@outlook.com>
2022-11-23 23:34:00 -0500

    translating non-english markdowns in parallel

--- main.py
+++ main.py
@@ -1,81 +1,10 @@
 # Importing libraries.
-import json
 import pandas as pd
 import os
-from langdetect import detect
-import re
-from html.parser import HTMLParser
-from io import StringIO
-import translators as ts
+from utils import *
 
 # Defining the main path.
 main_path = '/Users/ozyurtf/Documents/data/ai4code/'
-
-
-def detect_language(text_list):
-    try:
-        joined_texts = ' '.join(text_list)
-        detected_language = detect(joined_texts)
-    except:
-        detected_language = 'N/A'
-    return detected_language
-
-
-class HTMLStripper(HTMLParser): 
-    def __init__(self):
-        super().__init__()
-        self.reset()
-        self.strict = False
-        self.convert_charrefs = True
-        self.text = StringIO()
-
-    def handle_data(self, d):
-        self.text.write(d)
-
-    def get_data(self):
-        return self.text.getvalue()
-
-
-def clean_markdown(text, puncts_to_remove = '!"#$%&\*+-/;<>\\^_`|~'):
-    html_s = HTMLStripper()
-    html_s.feed(text)
-
-    text_tags_stripped = html_s.get_data()
-    text_encoded = text_tags_stripped
-
-    translator = str.maketrans(puncts_to_remove, ' ' * len(puncts_to_remove))
-
-    text_cleaned = text_encoded.translate(translator)
-    text_cleaned = re.sub(' +', ' ', text_cleaned)
-
-    return text_cleaned
-
-
-def extract_shuffled_codes_markdowns(train_or_test, file_id):
-    # Importing codes/markdowns data.
-    with open(main_path + '{}/{}.json'.format(train_or_test, file_id)) as f:
-        data = json.load(f)
-
-    # Extracting shuffled cell ids, cell types, and codes/markdowns, and detecting markdown languages.
-    cells_shuffled = list(data['cell_type'].keys())
-    cells_shuffled_joined = ' '.join(cells_shuffled)
-    cell_types_shuffled = list(data['cell_type'].values())
-    codes_markdowns_shuffled = []
-    markdowns_shuffled = []
-    for k in data['cell_type'].keys():
-        cell_type = data['cell_type'][k]
-        if cell_type == 'markdown':
-            cleaned_markdowns = clean_markdown(data['source'][k])
-            codes_markdowns_shuffled.append(cleaned_markdowns)
-            markdowns_shuffled.append(cleaned_markdowns)
-        else:
-            codes = data['source'][k]
-            codes_markdowns_shuffled.append(codes)
-    markdown_language = detect_language(markdowns_shuffled)
-
-    # Returning the shuffled cell ids, cell types, codes/markdowns, and markdown languages.
-    return cells_shuffled_joined, cell_types_shuffled, codes_markdowns_shuffled, markdown_language
-
 
 # Importing ancestors data.
 train_ancestors = pd.read_csv(main_path + 'train_ancestors.csv')
